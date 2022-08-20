ca39ebb335fd959b3b09bf7a4b4c266ebb0e32b8
Artem Balanovsky <mr.balanovsky@gmail.com>
2022-08-20 17:05:00 +0300

    Logger service added

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express, { Express } from 'express';
 import { userRouter } from './users/users';
 import { Server } from 'http';
+import { LoggerService } from './logger/logger.service';
+import { Logger } from 'tslog';
 
 
 export class App {
     app: Express;
     server: Server;
     port: number;
+    logger: LoggerService;
 
-    constructor() {
+    constructor(logger: LoggerService) {
         this.app = express();
         this.port = 8000;
+        this.logger = logger;
 
     }
 
@@ -21,7 +25,7 @@ export class App {
     public async init() {
         this.useRoutes();
         this.server = this.app.listen(this.port);
-        console.log(`Сервер запущен на http://localhost:${this.port}`);
+        this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
 
     }
 }
