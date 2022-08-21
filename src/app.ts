4bc469e0287e763de136e68efa5fd8ba27b47865
Artem Balanovsky <mr.balanovsky@gmail.com>
2022-08-21 21:05:32 +0300

    Error listener added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express';
 import { Server } from 'http';
+import { ExemptionFilter } from './errors/exemption.filter';
 import { LoggerService } from './logger/logger.service';
 import { UserController } from './users/users.controller';
 
@@ -10,15 +11,18 @@ export class App {
     port: number;
     logger: LoggerService;
     userController: UserController;
+    exemptionFilter: ExemptionFilter;
 
     constructor(
         logger: LoggerService,
-        userController: UserController
-        ) {
+        userController: UserController,
+        exemptionFilter: ExemptionFilter
+ ) {
         this.app = express();
         this.port = 8000;
         this.logger = logger;
         this.userController = userController;
+        this.exemptionFilter = exemptionFilter;
 
     }
 
@@ -26,8 +30,13 @@ export class App {
         this.app.use('/users', this.userController.router);
     }
 
+    useExemptionFilter() {
+        this.app.use(this.exemptionFilter.catch.bind(this.exemptionFilter));
+    }
+
     public async init() {
         this.useRoutes();
+        this.useExemptionFilter();
         this.server = this.app.listen(this.port);
         this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
 
