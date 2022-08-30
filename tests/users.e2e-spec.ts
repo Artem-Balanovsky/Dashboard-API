d4cab8c77152ff8c97b3ef0629f20f37377dfec8
Artem Balanovsky <mr.balanovsky@gmail.com>
2022-08-30 12:14:33 +0300

    e2e tests implemented

--- tests/users.e2e-spec.ts
+++ tests/users.e2e-spec.ts
@@ -12,8 +12,37 @@ describe('Users e2e', () => {
 	it('Register - error', async () => {
 		const res = await request(application.app)
 			.post('/users/register')
-			.send({ email: 'a@a.ru', password: '1' });
+			.send({ email: '1@gmail.com', password: 'wrong' });
 		expect(res.statusCode).toBe(422);
+	});
+
+	it('Login - success', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: '1@gmail.com', password: '1' });
+		expect(res.body.jwt).not.toBeUndefined();
+	});
+
+	it('Login - error', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: '1@gmail.com', password: 'wrong' });
+		expect(res.statusCode).toBe(401);
+	});
+
+	it('Info - success', async () => {
+		const login = await request(application.app)
+			.post('/users/login')
+			.send({ email: '1@gmail.com', password: '1' });
+		const res = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer ${login.body.jwt}`);
+		expect(res.body.email).toBe('1@gmail.com');
+	});
+
+	it('Info - error', async () => {
+		const res = await request(application.app).get('/users/info').set('Authrization', `Bearer 1`);
+		expect(res.statusCode).toBe(401);
 	});
 });
 
